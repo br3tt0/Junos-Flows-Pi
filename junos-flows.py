d2df2df28061d43382519878388705ec049a467d
br3tt0 <brett.lindsay@au.harveynorman.com>
2018-04-07 13:12:06 +1000

    begin build of the SecurityConfig object

--- junos-flows.py
+++ junos-flows.py
@@ -1,2 +1,17 @@
-import xml.etree.ElementTree as xml_tree:
+import xml.etree.ElementTree as xml_tree
+
+config_file = 'AUDCCFOIF003XML.xml'
+
+
+class SecurityConfig(config_file):
+    def __init__(self):
+        self.logs = []
+        self.alg = []
+        self.flow = []
+        self.nat = []
+        self.policies = []
+        self.zones = []
+        config_tree = xml_tree.parse(config_file)
+        config_root = config_tree.getroot(config_file)
+
 
