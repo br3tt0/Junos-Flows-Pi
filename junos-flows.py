ea0b439877edd31160ab18dc1202a038a818cfe9
br3tt0 <brett.lindsay@au.harveynorman.com>
2018-04-07 13:29:28 +1000

    Added SecurityConfig initalisation frames funtion

--- junos-flows.py
+++ junos-flows.py
@@ -5,16 +5,31 @@ config_file = 'AUDCCFOIF003XML.xml'
 
 class SecurityConfig:
     def __init__(self, config_file):
-        self.log = []
-        self.alg = []
-        self.flow = []
-        self.nat = []
-        self.policies = []
-        self.zones = []
+        self.log = None
+        self.alg = None
+        self.flow = None
+        self.nat = None
+        self.policies = None
+        self.zones = None
         config_tree = xml_tree.parse(config_file)
         config_root = config_tree.getroot(config_file)
 
-    def get_log(self, config_root):
+    def _init_log(self, config_root):
+        pass
+
+    def _init_alg(self, config_root):
+        pass
+
+    def _init_flow(self, config_root):
+        pass
+
+    def _init_nat(self, config_root):
+        pass
+
+    def _init_policies(self, config_root):
+        pass
+
+    def _init_zones(self, config_root):
         pass
 
 
