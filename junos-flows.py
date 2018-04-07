5fe92e841c63bf1ef416aef859f409eb6fd4e2b3
br3tt0 <brett.lindsay@au.harveynorman.com>
2018-04-07 13:20:38 +1000

    fixed SecurityConfig constructor bug

--- junos-flows.py
+++ junos-flows.py
@@ -3,9 +3,9 @@ import xml.etree.ElementTree as xml_tree
 config_file = 'AUDCCFOIF003XML.xml'
 
 
-class SecurityConfig(config_file):
-    def __init__(self):
-        self.logs = []
+class SecurityConfig:
+    def __init__(self, config_file):
+        self.log = []
         self.alg = []
         self.flow = []
         self.nat = []
@@ -13,5 +13,8 @@ class SecurityConfig(config_file):
         self.zones = []
         config_tree = xml_tree.parse(config_file)
         config_root = config_tree.getroot(config_file)
+
+    def get_log(self, config_root):
+        pass
 
 
